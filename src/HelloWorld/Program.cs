4aef4b822bd8861594c30d1ea8722c2286a2a200
Luiz Carneiro <luiz@carneiro.dev>
2025-09-12 10:24:14 +0200

    Initial trials with c#

---
+++ src/HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Hello, World!";
+            Console.WriteLine(a);
+            string? userInput = Console.ReadLine();
+            Console.WriteLine("You wrote... " + userInput);
+        }
+    }
+}
